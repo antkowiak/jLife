4b7e51c9905ee7737f5fff20e861c43cc08c7bdf
Ryan Antkowiak <antkowiak@gmail.com>
2015-09-15 17:24:18 -0500

    Create Main.java

---
+++ src/com/ryanantkowiak/jLife/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ryanantkowiak.IBAPI;
+
+
+/**
+ * @author Ryan Antkowiak (antkowiak@gmail.com)
+ *
+ */
+public class Main
+{
+	public static void main(String [] args)
+	{
+	}
+}
